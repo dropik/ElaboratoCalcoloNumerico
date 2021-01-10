06365af0abfaa93076266db42bc8b8b7adfbe796
Daniil Ryzhkov <drop.sovet@gmail.com>
2021-01-10 14:37:41 +0100

    Moved radn implementation to class

---
+++ +esercizio4/Esercizio.m
@@ -0,0 +1,52 @@
+classdef Esercizio
+    properties
+        x
+        n
+    end
+    
+    methods
+        function this = Esercizio(x, n)
+            this.x = x;
+            this.n = n;
+        end
+        
+        function root = radn(this)
+            if (this.isTrivialResult())
+                root = this.x;
+                return;
+            end
+            root = this.executeRootCalc();
+        end
+    end
+    
+    methods(Access = private)
+        function isTrivial = isTrivialResult(this)
+            isTrivial = false;
+            if ((this.x == 0) || (this.x == 1) || (this.n == 1))
+                isTrivial = true;
+            end
+        end
+        
+        function xi = executeRootCalc(this)
+            xi = this.x;
+            while (1)
+                f = this.getFuncValue(xi);
+
+                if (f < eps)
+                    break;
+                end
+
+                g = this.getDerivativeValue(xi);
+                xi = xi - f / g;
+            end
+        end
+        
+        function f = getFuncValue(this, xi)
+            f = xi^this.n - this.x;
+        end
+        
+        function g = getDerivativeValue(this, xi)
+            g = this.n * xi^(this.n - 1);
+        end
+    end
+end
--- +esercizio4/radn.m
+++ +esercizio4/radn.m
@@ -1,32 +1,14 @@
+function root = radn(x, n)
 % Using Newton's method to calculate n-th root.
 % Let x be the n-th root of a value A, then we
 % have to find a positive real root of function
 % f(x) = x^n - A
 % Applying Newton's method, we get for each new iteration
-% x.k+1 = 1/n((n-1)*x.k + A / x.k^(n-1))
-% which we could rewrite like
-% x.k+1 = x.k + deltaX.k
-% And to obtain the best precision possible, we stop
-% the algorithm when x.k+1 = x.k, or deltaX.k < epsilonMachine.
-function root = radn(x, n)
-    if ((x == 0) || (x == 1))
-        root = x;
-        return;
-    end
+% x.k+1 = x.k - f(x.k)/f'(x.k)
+% where f(x.k) = x.k^n - A,
+% ans f'(x.k) = n * x.k^(n-1);
+% And we stop the algorithm when f(x.k) < epsilon
 
-    % guessing initial value
-    root = 100;
-    
-    % execute algorithm
-    while 1
-        % calculating iteration
-        next = 1/n*((n - 1)*root + x/(root^(n - 1)));
-        
-        % exit on best precision
-        if next == root
-            break;
-        end
-        
-        root = next;
-    end
+    esercizio = esercizio4.Esercizio(x, n);
+    root = esercizio.radn();
 return
